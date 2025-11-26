b46618fdc3fd54193635257153047fc6895f71ce
Jennifer Cafiero <jcafiero2@gmail.com>
2025-11-26 10:22:11 -0500

    add favicon and prevent FOUC on page load

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,9 @@ export default defineConfig({
       },
     }),
   ],
+  server: {
+    headers: {
+      'Cache-Control': 'public, max-age=31536000', // 1 year for static assets
+    },
+  },
 })
