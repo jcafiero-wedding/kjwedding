0189e6ed101bdf99a57ad0727aea71ba6c029f08
Jennifer Cafiero <jcafiero2@gmail.com>
2026-01-07 09:48:06 -0500

    add last names

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,15 +1,18 @@
 function Home() {
   const todaysDate = new Date();
-  const weddingDate = new Date('2026-09-12');
+  const weddingDate = new Date("2026-09-12");
   const timeDifference = weddingDate.getTime() - todaysDate.getTime();
 
   return (
     <div className="hero">
       <div className="hero-content">
-        <h1>Kyle + Jennifer</h1>
+        <h1>Kyle Richers + Jennifer Cafiero</h1>
         <p>September 12, 2026</p>
         <p>Beach Haven, NJ</p>
-        <p>{Math.ceil(timeDifference / (1000 * 60 * 60 * 24))} days until our wedding!</p>
+        <p>
+          {Math.ceil(timeDifference / (1000 * 60 * 60 * 24))} days until our
+          wedding!
+        </p>
       </div>
     </div>
   );
